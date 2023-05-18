6aff9a1bf8036b4b863905fecdebbff24f7d6f48
roman-mykytka <roman.mykytka@binary-studio.com>
2023-05-18 20:50:15 +0300

    Section 2 | 8. Jest assertions and matchers

--- src/app/utils.ts
+++ src/app/utils.ts
@@ -1,1 +1,18 @@
-export const toUpperCase = (args: string) => args.toUpperCase();
+export type StringInfo = {
+    lowerCase: string;
+    upperCase: string;
+    characters: string[];
+    length: number;
+    info?: object;
+}
+
+export const toUpperCase = (args: string): string => args.toUpperCase();
+
+
+export const getStringInfo = (args: string): StringInfo => ({
+    upperCase: args.toUpperCase(),
+    lowerCase: args.toLowerCase(),
+    length: args.length,
+    characters: Array.from(args),
+    info: {}
+});
--- src/test/utils.test.ts
+++ src/test/utils.test.ts
@@ -1,4 +1,4 @@
-import {toUpperCase} from "../app/utils";
+import {toUpperCase, getStringInfo} from "../app/utils";
 
 describe('Utils tests', () => {
     it('should return uppercase of valid test', () => {
@@ -12,4 +12,28 @@ describe('Utils tests', () => {
         // assert
         expect(actual).toBe(expected)
     })
+
+    it.only('should return info about valid string', () => {
+        const sut = getStringInfo;
+
+        const actual = sut('My string');
+
+        expect(actual.lowerCase).toBe('my string');
+        expect(actual.info).toEqual({});
+
+        expect(actual.characters.length).toBe(9);
+        expect(actual.characters).toHaveLength(9);
+        expect(actual.characters).toEqual(['M', 'y', ' ', 's', 't', 'r', 'i', 'n', 'g']);
+        expect(actual.characters).toContain<string>('M');
+        expect(actual.characters).toEqual<string>(
+            expect.arrayContaining(['s', 't', 'r', 'i','M', 'y', ' ','n', 'g'])
+        );
+
+        expect(actual.info).not.toBe(undefined);
+        expect(actual.info).not.toBeUndefined();
+        expect(actual.info).toBeDefined();
+        expect(actual.info).toBeTruthy();
+
+    })
+
 })
