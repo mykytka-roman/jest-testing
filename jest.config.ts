18087d916ed7a45d840727280659946dd7c00f91
roman-mykytka <roman.mykytka@binary-studio.com>
2023-05-23 08:39:55 +0300

    Section 4 | 21. PassChecker setup

--- jest.config.ts
+++ jest.config.ts
@@ -1,12 +1,18 @@
 import type {Config} from '@jest/types';
 
+const baseDir = '<rootDir>/src/app/password-checker';
+const baseTestDir = '<rootDir>/src/test/password-checker';
+
 const config : Config.InitialOptions = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   verbose: true,
   collectCoverage: true,
   collectCoverageFrom: [
-    '<rootDir>/src/app/**/*.ts'
+    `${baseDir}/**/*.ts`
+  ],
+  testMatch: [
+    `${baseTestDir}/**/*.ts`
   ]
 };
 
---
+++ src/app/password-checker/password-checker.ts
@@ -0,0 +1,6 @@
+export class PasswordChecker {
+
+    public checkPassword(){
+
+    }
+}
