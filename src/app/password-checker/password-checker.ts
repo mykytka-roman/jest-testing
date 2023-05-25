fd43596ff8fb1a4cb9a28781884767656fd87996
roman-mykytka <roman.mykytka@binary-studio.com>
2023-05-25 17:19:10 +0300

    Section 4 | 22. PassChecker Iteration 1

--- src/app/password-checker/password-checker.ts
+++ src/app/password-checker/password-checker.ts
@@ -1,6 +1,16 @@
 export class PasswordChecker {
 
-    public checkPassword(){
-
+    public checkPassword(pass: string): boolean {
+        if (pass.length < 8) {
+            return false;
+        }
+        if(pass === pass.toLowerCase()) {
+            return false;
+        }
+        if(pass === pass.toUpperCase()) {
+            return false;
+        }
+        return true;
+        // return pass.length >= 8;
     }
 }
--- src/test/password-checker/password-checker.test.ts
+++ src/test/password-checker/password-checker.test.ts
@@ -9,9 +9,35 @@ describe('PasswordChecker test suite', ()=>{
         sut = new PasswordChecker();
     })
 
-    it('Should do nothing for the moment', ()=>{
-        sut.checkPassword();
+    it('Password with less than 8 chars is invalid', ()=>{
+       const actual =  sut.checkPassword('0123456');
+       expect(actual).toBe(false);
     })
+
+    it('Password must contain at least 8 characters', () => {
+        const actual = sut.checkPassword('01234567Ab');
+        expect(actual).toBe(true);
+    });
+
+    it('A password without an uppercase letter is invalid', () => {
+        const actual = sut.checkPassword('01234abc');
+        expect(actual).toBe(false);
+    });
+
+    it('A password which contains at least one uppercase letter is valid', () => {
+        const actual = sut.checkPassword('01234abcD');
+        expect(actual).toBe(true);
+    });
+
+    it('A password without an lowercase letter is invalid', () => {
+        const actual = sut.checkPassword('01234ABC');
+        expect(actual).toBe(false);
+    });
+
+    it('A password which contains at least one lowercase letter is valid', () => {
+        const actual = sut.checkPassword('01234ABCd');
+        expect(actual).toBe(true);
+    });
 
 
 })
