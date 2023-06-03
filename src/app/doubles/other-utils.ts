6cd604a66eb82bddc42bbd00b8e5bf016caeabf9
roman-mykytka <roman.mykytka@binary-studio.com>
2023-06-03 10:35:15 +0300

    Section 5 | 28. Fakes

--- src/app/doubles/other-utils.ts
+++ src/app/doubles/other-utils.ts
@@ -4,8 +4,19 @@ export type StringInfo = {
     characters: string[],
     length: number,
     extraInfo: Object | undefined
-}
+};
+
+type LoggerServiceCallBack = (arg: string) => void;
 
 export function calculateComplexity(stringInfo: Pick<StringInfo, 'extraInfo' | 'length'>) {
     return Object.keys(stringInfo.extraInfo).length * stringInfo.length;
-}
+};
+
+export function toUpperCaseWithCb(arg: string, callBack:LoggerServiceCallBack){
+    if(!arg) {
+        callBack('Invalid argument!');
+        return;
+    }
+    callBack(`called function with ${arg}`)
+    return arg.toUpperCase();
+};
